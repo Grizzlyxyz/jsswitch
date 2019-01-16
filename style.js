895c7f445f8e83fcd24318a79ec5d45a52d36eb1
Grizzly <grizzlyxyz@gmail>
2019-01-16 15:24:34 +0100

    sw

---
+++ style.js
@@ -0,0 +1,20 @@
+let leSwitch = (p) => {
+    switch(p) {
+      case 'oui':
+        alert('Tu as répondu oui');
+        break;
+      case 'non':
+        alert('Tu as répondu non');
+        break;
+      case 'bonjour':
+        alert('bienvenue');
+        break;
+      case 'aurevoir':
+        alert('à bientôt');
+        break;
+      default:
+        alert('Je ne comprends pas');
+    }
+  }
+
+  
